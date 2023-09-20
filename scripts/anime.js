42ffc73777d84fe04ac50bd1d907c9897abf6f36
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2023-09-20 18:05:22 +0700

    Update anime.js

--- scripts/anime.js
+++ scripts/anime.js
@@ -1,70 +1,36 @@
 // Danh sách phim
 const movies = [
 	{
-		title: "Swort Art Online",
-		subtitle: "Alicization",
-		year: "2021",
+		title: "Sword Art Online",
+		subtitle: "Progressive Movie - Kuraki Yuuyami no Scherzo",
+		year: "2022",
 		poster:
 			"https://ecdn.game4v.com/g4v-content/uploads/2022/08/03092132/SAO-P-1-game4v-1659493290-81.jpg",
 	},
 	{
 		title: "One Punch Man",
-		subtitle: "Season 2",
-		year: "2022",
+		subtitle: "Road to Hero",
+		year: "2015",
 		poster:
 			"https://cdn.europosters.eu/image/750/posters/one-punch-man-destruction-i61133.jpg",
 	},
 	{
-		title: "Onima: I'm now your sister!",
-		subtitle: "Season 2",
+		title: "Onimai: I'm now your sister!",
+		subtitle: "2023",
 		year: "2023",
 		poster:
 			"https://image.tmdb.org/t/p/original/tpvVSr6ThPQFISYc9xRVR7MKjDF.jpg",
 	},
 	{
-		title: "Miss Kobayashi's Dragon Maid",
-		subtitle: "Season 2",
+		title: "Miss Kobayashi's Dragon Maid S",
+		subtitle: "2021",
 		year: "2021",
 		poster: "https://cdn-amz.woka.io/images/I/811AMdAUtTL.jpg",
 	},
 	{
 		title: "Your Name",
-		subtitle: "Season 2",
-		year: "2022",
-		poster:
-			"https://egoamo.co.za/cdn/shop/products/your_name_anime_movie_poster_egoamo_posters_800x.jpg",
-	},
-	{
-		title: "Swort Art Online",
-		subtitle: "Alicization",
-		year: "2021",
-		poster:
-			"https://ecdn.game4v.com/g4v-content/uploads/2022/08/03092132/SAO-P-1-game4v-1659493290-81.jpg",
-	},
-	{
-		title: "One Punch Man",
-		subtitle: "Season 2",
-		year: "2022",
-		poster:
-			"https://cdn.europosters.eu/image/750/posters/one-punch-man-destruction-i61133.jpg",
-	},
-	{
-		title: "Onima: I'm now your sister!",
-		subtitle: "Season 2",
-		year: "2023",
-		poster:
-			"https://image.tmdb.org/t/p/original/tpvVSr6ThPQFISYc9xRVR7MKjDF.jpg",
-	},
-	{
-		title: "Miss Kobayashi's Dragon Maid",
-		subtitle: "Season 2",
-		year: "2021",
-		poster: "https://cdn-amz.woka.io/images/I/811AMdAUtTL.jpg",
-	},
-	{
-		title: "Your Name",
-		subtitle: "Season 2",
-		year: "2022",
+		subtitle: "2016",
+		year: "2016",
 		poster:
 			"https://egoamo.co.za/cdn/shop/products/your_name_anime_movie_poster_egoamo_posters_800x.jpg",
 	},
@@ -89,7 +55,7 @@ function displayMovies(movies) {
 
 		const movieTitle = document.createElement("h3");
 		movieTitle.textContent = movie.title;
-		
+
 		const subTitle = document.createElement("p");
 		subTitle.textContent = movie.subtitle;
 
@@ -118,15 +84,15 @@ function displayMovies(movies) {
 
 // Hàm lọc phim theo năm
 function filterMoviesByYear(selectedYear) {
-    let filteredMovies;
+	let filteredMovies;
 
-    if (selectedYear === "all") {
-        filteredMovies = movies;
-    } else {
-        filteredMovies = movies.filter(
-            (movie) => movie.year.toString() === selectedYear.toString()
-        );
-    }
+	if (selectedYear === "all") {
+		filteredMovies = movies;
+	} else {
+		filteredMovies = movies.filter(
+			(movie) => movie.year.toString() === selectedYear.toString()
+		);
+	}
 
-    displayMovies(filteredMovies);
+	displayMovies(filteredMovies);
 }
